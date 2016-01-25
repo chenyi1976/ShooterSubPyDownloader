3750a05cd3950202d0de454658bf4ba1e7f4d6bc
chenyi1976 <yichen1976@gmail.com>
2016-01-25 17:35:44 +1100

    full path; exception catch

--- Shooter.py
+++ Shooter.py
@@ -5,6 +5,7 @@ Created on Jan 20, 2014
 @author: magic282
 '''
 
+import traceback
 from SVPlayerHash import SVPlayerHash
 try:  # Python 3
     from urllib.parse import urlencode
@@ -29,31 +30,36 @@ class Shooter(object):
 
     def start(self):
         self.__videoHash = SVPlayerHash.ComputeFileHash(self.__fileName)
-        values = dict(filehash=self.__videoHash, pathinfo=self.__fileName, format="json", lang="Chn")
-        data = urlencode(values).encode('utf-8', 'replace')
-        req = Request(self.__SHOOTERURL, data)
-        rsp = urlopen(req)
-        content = rsp.read().decode('utf-8', 'replace')
 
-        jsonContent = json.loads(content)
-        for idx_i, i in enumerate(jsonContent):
-            print(i)
+        try:
+            values = dict(filehash=self.__videoHash, pathinfo=self.__fileName, format="json", lang="Chn")
+            data = urlencode(values).encode('utf-8', 'replace')
+            req = Request(self.__SHOOTERURL, data)
+            rsp = urlopen(req)
+            content = rsp.read().decode('utf-8', 'replace')
 
-            if i["Delay"] != 0:
-                delayFileName = '.'.join((self.__fileName, "chn%s" % ("" if idx_i == 0 else idx_i), "delay"))
-                with open(delayFileName, 'w') as output:
-                    output.write(str(i["Delay"]))
-            for idx_j, j in enumerate(i["Files"]):
-                outFileNameList = [self.__fileName, "chn%s" % ("" if idx_i == 0 else idx_i), str(j["Ext"])]
-                if len(i["Files"]) != 1:
-                    outFileNameList.insert(2, str(idx_j))
-                outFileName = '.'.join(outFileNameList)
-                dLink = j["Link"]
-                print(dLink)
-                response = urlopen(dLink)
-                backF = response.read()
-                with open(outFileName, 'wb') as output:
-                    output.write(backF)
+            jsonContent = json.loads(content)
+            for idx_i, i in enumerate(jsonContent):
+                print(i)
+
+                if i["Delay"] != 0:
+                    delayFileName = '.'.join((self.__fileName, "chn%s" % ("" if idx_i == 0 else idx_i), "delay"))
+                    with open(delayFileName, 'w') as output:
+                        output.write(str(i["Delay"]))
+                for idx_j, j in enumerate(i["Files"]):
+                    outFileNameList = [self.__fileName, "chn%s" % ("" if idx_i == 0 else idx_i), str(j["Ext"])]
+                    if len(i["Files"]) != 1:
+                        outFileNameList.insert(2, str(idx_j))
+                    outFileName = '.'.join(outFileNameList)
+                    dLink = j["Link"]
+                    print(dLink)
+                    response = urlopen(dLink)
+                    backF = response.read()
+                    with open(outFileName, 'wb') as output:
+                        output.write(backF)
+        except:
+            print '---- failed {} '.format(self.__fileName)
+            print traceback.print_stack()
 
     def __init__(self, params):
         '''
--- folder.py
+++ folder.py
@@ -9,9 +9,11 @@ def scan_folder(folder):
 
     for movie_file in movie_files:
         print '++++ subtitle for movie file: {}'.format(movie_file)
-        shooter = Shooter(movie_file)
+        movie_file_full_path = os.path.join(folder, movie_file)
+        shooter = Shooter(movie_file_full_path)
         shooter.start()
 
     sub_folders = [f for f in os.listdir(folder) if os.path.isdir(os.path.join(folder, f))]
     for sub_folder in sub_folders:
-        scan_folder(sub_folder)
+        sub_folder_full_path = os.path.join(folder, sub_folder)
+        scan_folder(sub_folder_full_path)
