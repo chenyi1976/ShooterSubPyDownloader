7d41d5e1f7dfc9ce9817cf87c3c6d65033d99fb7
chenyi1976 <yichen1976@gmail.com>
2016-01-25 19:04:52 +1100

    if subtitle already get, ignore it.

--- folder.py
+++ folder.py
@@ -11,8 +11,17 @@ def scan_folder(folder, detail=None):
     movie_files = [f for f in os.listdir(folder) if f.endswith('.mp4') or f.endswith('.mkv') or f.endswith('.avi')]
 
     for movie_file in movie_files:
-        print '++++ subtitle for movie file: {}'.format(movie_file)
         movie_file_full_path = os.path.join(folder, movie_file)
+
+        # if subtitle already exist, ignore it
+        srt_full_path = os.path.join(folder, movie_file, '.chn.srt')
+        ass_full_path = os.path.join(folder, movie_file, '.chn.ass')
+        if os.path.exists(srt_full_path) or os.path.exists(ass_full_path):
+            print '---- subtitle already get: {}'.format(movie_file)
+            continue
+
+        print '++++ subtitle for movie file: {}'.format(movie_file)
+
         shooter = Shooter(movie_file_full_path)
         count = shooter.start()
         detail.append((movie_file_full_path, count))
